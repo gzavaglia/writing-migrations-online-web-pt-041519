561e19d48104a6ec264b9980f99eef4065d29336
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-29 21:08:00 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,8 @@
 class Student < ActiveRecord::Base
+  attr_accessor :name, :grade, :birthdate
+  def initialize(name, grade, birthdate)
+    @name = name
+    @grade = grade
+    @birthdate = birthdate
+  end
 end
