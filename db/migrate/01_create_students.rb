42a344f928043cc88aa52a96002520d1302b680f
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-29 20:56:41 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -3,8 +3,5 @@ class CreateStudents < ActiveRecord::Migration[5.1]
     create_table :artists do 
       |t|
       t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
     end
 end
